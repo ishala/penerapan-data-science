2f7f7cca39127787617b31136658128d1fb971a5
ishala <faishalali22@students.amikom.ac.id>
2025-05-08 16:44:43 +0700

    Add pandas import and define template dataframes for curricular and grade data

--- modules/data_load.py
+++ modules/data_load.py
@@ -1,6 +1,8 @@
 import json
 import os
+import pandas as pd
 
+# Data JSON
 MODULE_DIR = os.path.dirname(__file__)
 
 JSON_PATH = os.path.join(MODULE_DIR, '..', 'data', 'jsons')
@@ -16,3 +18,23 @@ for file in file_list:
     
     with open(json_file_path, 'r') as json_f:
         json_list[key_name] = json.load(json_f)
+
+
+# Template Dataframe
+curricular_df = pd.DataFrame([
+    {'Curricular Type': '1st Semester Enrolled', 'Total Units': 0,},
+    {'Curricular Type': '1st Semester Approved', 'Total Units': 0},
+    {'Curricular Type': '1st Semester No Eval', 'Total Units': 0},
+    {'Curricular Type': '2nd Semester Enrolled', 'Total Units': 0},
+    {'Curricular Type': '2nd Semester Approved', 'Total Units': 0},
+    {'Curricular Type': '2nd Semester No Eval', 'Total Units': 0}
+])
+
+grade_df = pd.DataFrame([
+    {
+    '1st Semester Grade': 0.0,
+    '2nd Semester Grade': 0.0,
+    'Admission Grade': 0.0,
+    'Previous Qualification Grade': 0.0
+    }
+])
